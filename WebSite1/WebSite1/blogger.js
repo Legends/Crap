63055e044ed9ba90efe1008d24f58ca08d417275
Admin <Admin@BUSHIDO>
2018-02-06 14:20:43 +0100

    initial crap commit

---
+++ WebSite1/WebSite1/blogger.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(() => {
+    $("a", "body").each((idx, el) => {
+
+        var $a = $(el);
+        var href = $a.attr("href");
+        if (!href) return;
+        console.log(href);
+        var idx = href.indexOf("#");
+        if (idx !== -1) {
+            let newurl = href.substring(idx, (href.length - idx));
+            $a.attr("href", newurl);
+        }
+    });
+});
